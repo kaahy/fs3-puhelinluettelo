1e44037742c2ebcac0460d662cb5632d7ebf54e4
kaa <113045348+kaahy@users.noreply.github.com>
2024-01-15 20:26:01 +0200

    3b: 3.11

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))  //
 
 let persons = [
     {

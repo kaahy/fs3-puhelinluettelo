d725aa713ef65a56a9060229370a549b6408f98b
kaa <113045348+kaahy@users.noreply.github.com>
2024-01-15 19:02:32 +0200

    3b: 3.9

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
-
 const app = express()
+const cors = require('cors')
 
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
-
 app.use(express.json())
+app.use(cors())
 
 let persons = [
     {
